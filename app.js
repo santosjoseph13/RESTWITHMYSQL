ed73b33dd7670aa932c3c4df957a5e4529dbc254
santosjoseph13 <santosjoseph13@users.noreply.github.com>
2019-11-12 15:18:14 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+var bookRouter = require('./routes/actions');  //Import routes for "catalog" area of site
+
+app.use(morgan('short'))
+app.use('/actions', bookRouter);  // Add catalog routes to middleware chain.
+
+app.listen(4000,function(){
+console.log('listening');
+
+
+});
