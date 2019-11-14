5b83c9a2589efb1b1077a976daf99bd1a5518e4e
santosjoseph13 <santosjoseph13@users.noreply.github.com>
2019-11-14 10:04:31 +0800

    added del,up,index in account routes

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,15 @@
 var Book = require('../models/account');
 
+exports.accountindex = function (req,res){
+    res.send('showall');
+};
+
+exports.accountupdate_put = function (req,res){
+    res.send('update');
+};
+exports.accountdelete_post = function (req,res){
+    res.send('delete');
+};
 exports.accountcreate_post = function (req,res){
-    res.send('YAY');
+    res.send('create');
 };
--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var account_controller = require('../controllers/accountController.js');
 
+router.get('/', account_controller.accountindex);
 router.post('/create', account_controller.accountcreate_post);
-
+router.post('/update', account_controller.accountupdate_put);
+router.post('/delete', account_controller.accountdelete_post);
 
 module.exports = router;
